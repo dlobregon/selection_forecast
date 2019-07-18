87f580721164c8850838467e811794a398653863
Diego López <troyanrex@gmail.com>
2019-07-18 17:58:52 -0600

    creación de archivo y comentarios iniciales

---
+++ selection_simulation.R
@@ -0,0 +1,9 @@
+# simulación de una selección de candidato
+# esto quiere decir que no es necesariamente una elección ya que en "teoría" 
+# los actores que se están simulando no afectan una elección.
+
+# se utiliza el método de montecarlo para poder calcular la estimación de probabilidad
+# de que se escoga un determinado candidato
+
+# idea, se trata de que cada actor trate de escoger en base a quién considera mejor para 
+# mantener su estado.
